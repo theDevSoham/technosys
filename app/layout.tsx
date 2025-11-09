c254969132d1f3a91a0d67247d0292d3b071a6e8
Soham Das <publicsoham.24@gmail.com>
2025-11-09 21:20:46 +0530

    fix: hoizontal scroll fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen lg:w-[99vw]`}
       >
         <Navbar />
         {children}
