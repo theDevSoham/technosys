7554a393a5a6c58f13fbd72dc557b9f3cb9aa8a0
Soham Das <publicsoham.24@gmail.com>
2025-11-10 13:11:26 +0530

    feat: added industries served component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Graph from "@/components/page/homepage/Graph";
 import HeroSection from "@/components/page/homepage/HeroSection";
+import IndustriesServed from "@/components/page/homepage/IndustriesServed";
 import Latency from "@/components/page/homepage/Latency";
 import OurInfra from "@/components/page/homepage/OurInfra";
 import OurServices from "@/components/page/homepage/OurServices";
@@ -16,6 +17,7 @@ export const Home = () => {
       </div>
       <OurInfra />
       <SpeedTest />
+      <IndustriesServed />
     </main>
   );
 };
