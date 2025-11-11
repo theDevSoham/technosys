a89b32ba5c2adc56d958977361a296a74b000082
Soham Das <publicsoham.24@gmail.com>
2025-11-11 16:44:04 +0530

    fix: logo changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import MenuBar from "./Menubar";
 const Navbar = () => {
   return (
     <nav style={{ background: `${theme.colors.primary[500]}` }}>
-      <div className={`${container} flex justify-between items-center py-6 lg:py-3`}>
+      <div className={`${container} flex justify-between items-center py-4 lg:py-3`}>
         <div className="lg:w-72 w-60">
           <Image src="/Logo.png" width={300} height={88} alt="Logo image" />
         </div>
